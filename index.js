3902cdb7b7c2554fe6b8f3843acbd4d813eb414c
Jeloi <jeremyho19@gmail.com>
2014-09-14 21:07:50 -0700

    make input dynamic parameter, cleanup header

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var https = require('https');
+var querystring = require('querystring');
 
 function YodaSpeak(key) {
 	this._credentials ={
 		host: "yoda.p.mashape.com",
-		path: "/yoda?sentence=waaa",
-		headers: { "X-Mashape-Key": key || "" },
+		path: "/yoda",
+		headers: { "X-Mashape-Key": key || null },
 		// Mashape's certificate doesn't seem to be authorized.
 		// I'm gonna assume potential man-in-the-middle attacks for yoda-speak words is not too serious.
 		rejectUnauthorized: false 
@@ -12,6 +13,7 @@ function YodaSpeak(key) {
 }
 
 YodaSpeak.prototype.convert = function(text, callback) {
+	this._credentials.path = "/yoda?" + querystring.stringify({sentence: text});
 	var req = https.get(this._credentials, function(res) {
 		res.on('data', function (data) {
 	      callback(null, data);
