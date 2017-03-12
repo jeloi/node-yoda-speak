dd92265ca530e791c6cec4e094368e228f9f66cc
adumbidiot <nathaniel.daniel23@outlook.com>
2017-03-11 22:13:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-var https = require('https');
-var querystring = require('querystring');
+const soap = require('soap');
 
-function YodaSpeak(key) {
-	this._credentials ={
-		host: "yoda.p.mashape.com",
-		path: "/yoda",
-		headers: { "X-Mashape-Key": key || null },
-		// Mashape's certificate doesn't seem to be authorized.
-		// I'm gonna assume potential man-in-the-middle attacks for yoda-speak words is not too serious.
-		rejectUnauthorized: false 
-	}
-}
-
-YodaSpeak.prototype.convert = function(text, callback) {
-	this._credentials.path = "/yoda?" + querystring.stringify({sentence: text});
-	var req = https.get(this._credentials, function(res) {
-		res.on('data', function (data) {
-	      callback(null, data);
-	    });
-	})
-
-	req.on('error', function(err) {
-	  	callback(err);
+const convert = function(input, cb){
+	soap.createClient('https://www.yodaspeak.co.uk/webservice/yodatalk.php?wsdl', function(err, client){
+		if(err){
+			return cb(err);
+		}
+		client.yodaTalk({inputText: input}, function(err, result){
+			if(err){
+				return cb(err);
+			}
+			return cb(err, result.return);
+		});
 	});
 }
 
-
-module.exports = YodaSpeak;
+module.exports = function(){
+	this.convert = convert;
+}
